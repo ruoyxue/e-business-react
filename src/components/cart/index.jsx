b744b32d6e514fcf16db49aeee100a415c3b9a2b
xueruoyao <1642216761@qq.com>
2023-04-11 22:38:01 +0800

    modify

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,6 +1,7 @@
 import CartDropdown from "../cart-dropdown";
 import React, {useState, useContext} from 'react'
-import { CartContext } from "../../contexts/cart.context";
+import { CartContext } from "../../contexts/cart.context"
+import ShoppingBagImage from './src/assets/images/shopping_bag.png'
 
 export default function Cart() {
 	const [isCartOpen, setIsCartOpen] = useState(false)
@@ -22,7 +23,7 @@ export default function Cart() {
 	return (
 		<div id="cart" className="relative" onBlur={closeCartDropdown}>
 			<div tabIndex={0} onClick={clickHandler} className='relative hover:-translate-y-0.5 active:translate-y-0 transition-transform cursor-pointer'>
-				<img className='h-14' src="./src/assets/images/shopping_bag.png" alt="shopping bag" />
+				<img className='h-14' src={ShoppingBagImage} alt="shopping bag" />
 				<span className='Z-10 text-2xl font-bold absolute left-1/2 -translate-x-1/2 -translate-y-2/3' style={{top:'70%'}}>
 					{ cartItemCount }
 				</span>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,7 +6,7 @@ import {
 	signInAuthUserwithEmailAndPassword,
 } from '../../utils/firebase'
 import LoginStyle from './index.module.css'
-
+import GoogleImage from './src/assets/images/google.png'
 
 export default function Login() {
 	let emailRef = useRef()
@@ -58,7 +58,7 @@ export default function Login() {
 					<div className='mt-10 flex justify-center'>
 						<button type="submit" className={`${LoginStyle.login__button} mr-6`}>Log In</button>
 						<button className={`${LoginStyle.login__button} flex justify-center items-center`} onClick={LoginWithGoogle}>
-							<img className='h-6 w-6 mr-4 block opacity-70' src="./src/assets/images/google.png" alt="google icon" />
+							<img className='h-6 w-6 mr-4 block opacity-70' src={GoogleImage} alt="google icon" />
 							<span>Log In with Google</span>
 						</button>
 					</div>
