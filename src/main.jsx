e3b7b405cb28da8ea7f9fbd11413c46c9f6adb28
xueruoyao <1642216761@qq.com>
2023-04-17 22:06:22 +0800

    add persist redux storage

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './store/store'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
 	<Provider store={store}> 
-		<App />
-	</Provider>
-  </BrowserRouter>,
+		<PersistGate persistor={persistor}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</PersistGate>
+  	</Provider>
 )
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,14 +1,29 @@
-import  {configureStore } from "@reduxjs/toolkit"
+import  { configureStore, combineReducers } from "@reduxjs/toolkit"
 import userReducer from "./user"
 import categoriesReducer from './categories'
 import cartReducer from './cartItems'
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import thunk from 'redux-thunk'
 
-const store = configureStore({
-	reducer: {
-		user: userReducer,
-		categories: categoriesReducer,
-		cartItems: cartReducer
-	}
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user']
+}
+
+const rootReducer = combineReducers({
+	user: userReducer,
+	categories: categoriesReducer,
+	cartItems: cartReducer
 })
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	devTools: process.env.NODE_ENV !== 'production',
+	middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
